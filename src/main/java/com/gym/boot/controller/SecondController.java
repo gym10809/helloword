9588c273866d6c1c729d3b686a3936aeb60994eb
gym10809 <gym10809@163.com>
2022-06-27 19:45:34 +0800

    分支

---
+++ src/main/java/com/gym/boot/controller/SecondController.java
@@ -0,0 +1,16 @@
+package com.gym.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gym
+ * @create 2022/6/27 0027 19:42
+ */
+@RestController
+public class SecondController {
+    @RequestMapping("/second")
+    public String second(){
+        return "second";
+    }
+}
