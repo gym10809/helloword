c5139ae8f70f98b8bd076919fdfb48b50923b8bf
gym10809 <gym10809@163.com>
2022-06-27 20:02:56 +0800

    1

--- src/main/java/com/gym/boot/controller/SecondController.java
+++ src/main/java/com/gym/boot/controller/SecondController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecondController {
     @RequestMapping("/second")
     public String second(){
-        return "second+11111";
+        return "second";
     }
 }
